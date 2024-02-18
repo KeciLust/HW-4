9b9fec3faf764f25395d52aeea912e5f1cbce3ac
Denis <Dinya.nikitin@gmail.com>
2024-02-18 21:34:26 +0300

    finish 1.0.0

--- HW-4_1/index.js
+++ HW-4_1/index.js
@@ -1,15 +1,31 @@
 #!/usr/bin/env node
 
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
+const readline = require("node:readline");
+const { stdin: input, stdout: output } = require("node:process");
+const fs = require("fs");
+
+const path = require("path");
+
+const file = path.join(__dirname, "log.txt");
 
 const rl = readline.createInterface({ input, output });
 const num = Math.floor(Math.random() * 2 + 1);
 
-console.log(num)
-
-
-
-
-
-rl.close();
+console.log("Угадайте число");
+let result = "";
+rl.on("line", (ans) => {
+  if (num == ans) {
+    console.log("Вы угадали");
+    result = "1";
+    rl.close();
+  } else {
+    console.log("Вы ошиблись");
+    result = "0";
+    rl.close();
+  }
+  fs.appendFile(file, result, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
--- HW-4_2/index.js
+++ HW-4_2/index.js
@@ -1,2 +1,25 @@
 #!/usr/bin/env node
 
+const fs = require("fs");
+const path = require("path");
+const file = path.join(__dirname, "../hw-4_1/log.txt");
+const read = fs.readFile(file, "utf-8", (err, data) => {
+  if (err) {
+    console.log("Файла логов нет");
+    return;
+  }
+  let win = 0;
+  let lose = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] == 0) {
+      lose++;
+    } else {
+      win++;
+    }
+  }
+  console.log("Общее количество игры:", data.length);
+  console.log("Общее количество победных игр:", win);
+  console.log("Общее количество проишронных игр:", lose);
+  console.log("Отношение побед к поражениям:", win / lose);
+});
